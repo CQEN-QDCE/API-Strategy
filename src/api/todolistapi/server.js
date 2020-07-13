ead4081cfe9424ba940dde918f5aa67cd8a30aa1
Martin St-Pierre <martin.st-pierre@sct.gouv.qc.ca>
2020-07-13 14:32:34 -0400

    Amélioration connexion à la BD.

--- src/api/todolistapi/server.js
+++ src/api/todolistapi/server.js
@@ -4,10 +4,34 @@ var express = require('express'),
   mongoose = require('mongoose'),
   Task = require('./models/todoListModel'), //created model loading here
   bodyParser = require('body-parser');
-  
+
+const mongoUri = process.env.uri;
+const mongoUsername = process.env.username || process.env.MONGODB_USER;
+const mongoPassword = process.env.password || process.env.MONGODB_PASSWORD;
+const dbName = process.env.database_name || 
+			   process.env.MONGODB_DBNAME || 
+			   process.env.MONGODB_DATABASE ||
+			   'Tododb';
+const dbServiceName = process.env.DATABASE_SERVICE_NAME || 'localhost';
+
+var dbConnectionUrl;
+
+// If the monogo secret has been attached, modify the provided URI to include
+// authentication credentials
+if (mongoUri) {
+	var auth = mongoUsername + ':' + mongoPassword + '@'
+	var pieces = mongoUri.split('//');
+	dbConnectionUrl = pieces[0] + '//' + auth + pieces[1] + '/' + dbName;
+}
+else if (process.env.MONGODB_URL){
+	dbConnectionUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017/Tododb';
+} else {
+	dbConnectionUrl = 'mongodb://localhost:27017/' + dbName;
+}
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb', {useUnifiedTopology: true, useNewUrlParser: true}); 
+mongoose.connect(dbConnectionUrl, {useUnifiedTopology: true, useNewUrlParser: true}); 
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
